6f0f7d5cb0a16ed5d248f1d4bc9ec14be536c1e0
Roger <roger@apis3.com>
2022-02-07 12:06:34 -0300

    width menu fixed

--- assets/js/neovite.js
+++ assets/js/neovite.js
@@ -1,30 +1,44 @@
 window.onscroll = function(){
     fixedMenuDmri();
+    highlightMenu();
 }
 
 var menuDmri = document.getElementById('dmri-menu');
-var stickyDmri = menuDmri.offsetTop;
+menuDmri.style.maxWidth = menuDmri.offsetWidth + "px";
+menuDmri.style.minWidth = menuDmri.offsetWidth + "px";
 
 function fixedMenuDmri(){
-    if (window.pageYOffset > stickyDmri){
+    if (window.pageYOffset > menuDmri.offsetTop){
         menuDmri.classList.add('stickyDmri');
     } else {
         menuDmri.classList.remove('stickyDmri');
     }
 }
 
-$(window).scroll(function() {
+function highlightMenu(){
+    var position = document.documentElement.scrollTop;
+    //console.log('position',position);
 
-    var position = $(this).scrollTop();
+    var sections = document.querySelectorAll('.section');
+    sections.forEach( s => {
 
-    $('.section').each(function() {
-        var target = $(this).offset().top;
-        var id = $(this).attr('id');
+        var target = s.offsetTop;
+        var id = s.id;
+        //console.log('target',target)
 
-        if (position >= target) {
-            $('#dmri-menu li a').attr('href', id).addClass('active');
+        if (position >= target){
+            var menu = document.querySelectorAll('.dmri-menu-link');
+
+            menu.forEach(link => {
+                if(link.dataset.section == id){
+                    link.classList.add('active');
+                }
+            })
+
         }
+
     });
-});
+
+}
 
 // https://codepen.io/eksch/pen/xwdOeK
