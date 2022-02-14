fccc798bae1d9077ee0fd448275311019c81431f
Roger <roger@apis3.com>
2022-02-14 17:27:44 -0300

    correcao fontes

--- assets/js/neovite.js
+++ assets/js/neovite.js
@@ -59,6 +59,26 @@ menu.forEach(link=>{
         var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?section=' + id;
         window.history.pushState({path:newurl},'',newurl);
 
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+                if (xmlhttp.status == 200) {
+                    //console.log(xmlhttp.responseText);
+                    console.log('alright');
+                }
+                else if (xmlhttp.status == 400) {
+                    //console.log('There was an error 400');
+                }
+                else {
+                    //console.log('something else other than 200 was returned');
+                }
+            }
+        };
+
+        xmlhttp.open("GET", newurl, true);
+        xmlhttp.send(); 
+
     });
 
 });
